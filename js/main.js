0d2646ba49c992f13ac546d182a4395d4fa21f81
Rachel Barker <rachelbarker001@gmail.com>
2018-06-25 16:19:11 -0700

    add image cache

--- js/main.js
+++ js/main.js
@@ -1,45 +1,98 @@
 /*----- constants -----*/
+var stones = {
+    1: "https://imgur.com/sJjKMan",
+    2: "https://imgur.com/LrTwEib",
+    3: "https://imgur.com/ZGkoRJd",
+    4: "https://imgur.com/lrltXBm",
+    5: "https://imgur.com/USludbO",
+    6: "https://imgur.com/GCACIgn",
+    7: "https://imgur.com/TBIcaiD",
+    8: "https://imgur.com/dXYRsJp",
+    9: "https://imgur.com/STz6VAs",
+    10: "https://imgur.com/0Ie6D5X",
+    11: "https://imgur.com/gTsIOw8",
+    12: "https://imgur.com/Rbp3xXZ",
+    13: "https://imgur.com/LXtmNS9",
+    14: "https://imgur.com/qFSHgsS",
+    15: "https://imgur.com/S5Xvi3y",
+    16: "https://imgur.com/6XJbMSH",
+    17: "https://imgur.com/OdQCwnD",
+    18: "https://imgur.com/p2tydKx",
+    19: "https://imgur.com/iGXgdrH",
+    20: "https://imgur.com/ZCyWyPU",
+    21: "https://imgur.com/KvMZYH2",
+    22: "https://imgur.com/EYqjDQq",
+    23: "https://imgur.com/nzanJfr",
+    24: "https://imgur.com/BGh4dUr",
+    25: "https://imgur.com/8gHExcs",
+    26: "https://imgur.com/70SWrbd",
+    27: "https://imgur.com/20r5nXe",
+    28: "https://imgur.com/DVPXGb6",
+    29: "https://imgur.com/rUP96Kx",
+    30: "https://imgur.com/eLo2yLS",
+    31: "https://imgur.com/hl9pLdy",
+    32: "https://imgur.com/tYJUB4G",
+    33: "https://imgur.com/CmGveaF",
+    34: "https://imgur.com/KgdPfLv",
+    35: "https://imgur.com/wiH3Ol7",
+    36: "https://imgur.com/u6AuTvR",
+    37: "https://imgur.com/LbkiEco",
+    38: "https://imgur.com/tQMVlBU",
+    39: "https://imgur.com/1JKK0T5",
+    40: "https://imgur.com/c1J22AL",
+    41: "https://imgur.com/V36PCd7",
+    42: "https://imgur.com/cRWT3fV",
+    43: "https://imgur.com/Qmi2DWM",
+    44: "https://imgur.com/TQrGoUU",
+    45: "https://imgur.com/q2QxdUk",
+    46: "https://imgur.com/E9kbMn7",
+    47: "https://imgur.com/l1nXrxf",
+    48: "https://imgur.com/XAqOsPN"
+}
+
+
 
 
 /*----- app's state (variables) -----*/
-var board, score;
+var board = ['#h1', '#h2', '#h3', '#h4', '#h5', '#h6', '#p1-store', '#h8', '#h9', '#h10', '#h11', '#h12', '#h13', '#p2-store'];
+
 
 
 /*----- cached element references -----*/
-var scoreEl = document.querySelector('h1');
+// var scoreEl = document.querySelector('h1');
 
 
 
 /*----- event listeners -----*/
-document.getElementById('inc-btn').addEventListener('click',function() {
-    handleUpdateScore(1)
-});
-document.getElementById('dec-btn').addEventListener('click',function() {
-    handleUpdateScore(-1)
-});
+// document.getElementById('inc-btn').addEventListener('click',function() {
+//     handleUpdateScore(1)
+// });
+// document.getElementById('dec-btn').addEventListener('click',function() {
+//     handleUpdateScore(-1)
+// });
 
-/*----- functions -----*/
- function initialize() {
-    board = []
-    score = 0; 
-
-
-    //render is LAST!!
-    render();
- }
-
- //responsible for trasfering all state to the DOm
-
- function render() {
-    scoreEl.textContent = score;
-
- }
-
- function handleUpdateScore(diff) {
-    score += diff;
+// /*----- functions -----*/
+//  function initialize() {
+//     board = []
+//     score = 0; 
 
 
-    render();
- }
+//     //render is LAST!!
+//     render();
+//  }
+
+//  //responsible for trasfering all state to the DOm
+
+//  function render() {
+//     scoreEl.textContent = score;
+
+//  }
+
+//  function handleUpdateScore(diff) {
+//     score += diff;
+
+render() {
+
+}; 
 
  initialize();
