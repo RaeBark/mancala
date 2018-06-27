992fb5093d87c7dc71650ac6411a9f637db8dd87
Rachel Barker <rachelbarker001@gmail.com>
2018-06-27 16:57:51 -0700

    add styles through rendering

--- js/main.js
+++ js/main.js
@@ -1,53 +1,54 @@
 /*----- constants -----*/
 var stones = {
-    1: "https://imgur.com/sJjKMan",
-    2: "https://imgur.com/LrTwEib",
-    3: "https://imgur.com/ZGkoRJd",
-    4: "https://imgur.com/lrltXBm",
-    5: "https://imgur.com/USludbO",
-    6: "https://imgur.com/GCACIgn",
-    7: "https://imgur.com/TBIcaiD",
-    8: "https://imgur.com/dXYRsJp",
-    9: "https://imgur.com/STz6VAs",
-    10: "https://imgur.com/0Ie6D5X",
-    11: "https://imgur.com/gTsIOw8",
-    12: "https://imgur.com/Rbp3xXZ",
-    13: "https://imgur.com/LXtmNS9",
-    14: "https://imgur.com/qFSHgsS",
-    15: "https://imgur.com/S5Xvi3y",
-    16: "https://imgur.com/6XJbMSH",
-    17: "https://imgur.com/OdQCwnD",
-    18: "https://imgur.com/p2tydKx",
-    19: "https://imgur.com/iGXgdrH",
-    20: "https://imgur.com/ZCyWyPU",
-    21: "https://imgur.com/KvMZYH2",
-    22: "https://imgur.com/EYqjDQq",
-    23: "https://imgur.com/nzanJfr",
-    24: "https://imgur.com/BGh4dUr",
-    25: "https://imgur.com/8gHExcs",
-    26: "https://imgur.com/70SWrbd",
-    27: "https://imgur.com/20r5nXe",
-    28: "https://imgur.com/DVPXGb6",
-    29: "https://imgur.com/rUP96Kx",
-    30: "https://imgur.com/eLo2yLS",
-    31: "https://imgur.com/hl9pLdy",
-    32: "https://imgur.com/tYJUB4G",
-    33: "https://imgur.com/CmGveaF",
-    34: "https://imgur.com/KgdPfLv",
-    35: "https://imgur.com/wiH3Ol7",
-    36: "https://imgur.com/u6AuTvR",
-    37: "https://imgur.com/LbkiEco",
-    38: "https://imgur.com/tQMVlBU",
-    39: "https://imgur.com/1JKK0T5",
-    40: "https://imgur.com/c1J22AL",
-    41: "https://imgur.com/V36PCd7",
-    42: "https://imgur.com/cRWT3fV",
-    43: "https://imgur.com/Qmi2DWM",
-    44: "https://imgur.com/TQrGoUU",
-    45: "https://imgur.com/q2QxdUk",
-    46: "https://imgur.com/E9kbMn7",
-    47: "https://imgur.com/l1nXrxf",
-    48: "https://imgur.com/XAqOsPN"
+    0: "",
+    1: "https://i.imgur.com/sJjKMan.png",
+    2: "https://i.imgur.com/LrTwEib.png",
+    3: "https://i.imgur.com/ZGkoRJd.png",
+    4: "https://i.imgur.com/bz22JUK.png",
+    5: "https://i.imgur.com/USludbO.png",
+    6: "https://i.imgur.com/GCACIgn.png",
+    7: "https://i.imgur.com/TBIcaiD.png",
+    8: "https://i.imgur.com/dXYRsJp.png",
+    9: "https://i.imgur.com/STz6VAs.png",
+    10: "https://i.imgur.com/0Ie6D5X.png",
+    11: "https://i.imgur.com/gTsIOw8.png",
+    12: "https://i.imgur.com/Rbp3xXZ.png",
+    13: "https://i.imgur.com/LXtmNS9.png",
+    14: "https://i.imgur.com/qFSHgsS.png",
+    15: "https://i.imgur.com/S5Xvi3y.png",
+    16: "https://i.imgur.com/6XJbMSH.png",
+    17: "https://i.imgur.com/OdQCwnD.png",
+    18: "https://i.imgur.com/p2tydKx.png",
+    19: "https://i.imgur.com/iGXgdrH.png",
+    20: "https://i.imgur.com/ZCyWyPU.png",
+    21: "https://i.imgur.com/KvMZYH2.png",
+    22: "https://i.imgur.com/EYqjDQq.png",
+    23: "https://i.imgur.com/nzanJfr.png",
+    24: "https://i.imgur.com/BGh4dUr.png",
+    25: "https://i.imgur.com/8gHExcs.png",
+    26: "https://i.imgur.com/70SWrbd.png",
+    27: "https://i.imgur.com/20r5nXe.png",
+    28: "https://i.imgur.com/DVPXGb6.png",
+    29: "https://i.imgur.com/rUP96Kx.png",
+    30: "https://i.imgur.com/eLo2yLS.png",
+    31: "https://i.imgur.com/hl9pLdy.png",
+    32: "https://i.imgur.com/tYJUB4G.png",
+    33: "https://i.imgur.com/CmGveaF.png",
+    34: "https://i.imgur.com/KgdPfLv.png",
+    35: "https://i.imgur.com/wiH3Ol7.png",
+    36: "https://i.imgur.com/u6AuTvR.png",
+    37: "https://i.imgur.com/LbkiEco.png",
+    38: "https://i.imgur.com/tQMVlBU.png",
+    39: "https://i.imgur.com/1JKK0T5.png",
+    40: "https://i.imgur.com/c1J22AL.png",
+    41: "https://i.imgur.com/V36PCd7.png",
+    42: "https://i.imgur.com/cRWT3fV.png",
+    43: "https://i.imgur.com/Qmi2DWM.png",
+    44: "https://i.imgur.com/TQrGoUU.png",
+    45: "https://i.imgur.com/q2QxdUk.png",
+    46: "https://i.imgur.com/E9kbMn7.png",
+    47: "https://i.imgur.com/l1nXrxf.png",
+    48: "https://i.imgur.com/XAqOsPN.png"
 }
 
 var p1store = 6;
@@ -64,8 +65,8 @@ var board, turn, winner;
 
 
 /*----- cached element references -----*/
-
-
+var p1scoreDisplay = document.getElementById('p1-score');
+var p2scoreDisplay = document.getElementById('p2-score')
 
 /*----- event listeners -----*/
 
@@ -79,7 +80,7 @@ function initialize() {
     turn = 1;
     //render is LAST!!
     render();
-}
+};
 
 function handleMove(evt) {
     var idx = parseInt(evt.target.id.replace('hole', ''));
@@ -101,22 +102,18 @@ function handleMove(evt) {
 
     checkForClearedRow();
 
-    // if (board[p1holes].numStones === 0 && board[p2holes].numStones === 0) { 
-    //     checkForWinner(p1store, p2store);
-    // }
-
     changeTurn(lastHoleIdx);
 
     render();
-}
+};
 
 function isIndexOnPlayerSide(idx) {
     return turn === 1 ? p1holes.includes(idx) : p2holes.includes(idx);
-}
+};
 
 function getPlayersStoreIdx() {
     return turn === 1 ? p1store : p2store;
-}
+};
 
 function spreadStones(idx) {
     var numStones = board[idx];
@@ -142,14 +139,14 @@ function capture(lastHoleIdx) {
         board[getPlayersStoreIdx()] += board[opposite];
         board[opposite] = 0;
     }
-}
+};
 
 function checkForClearedRow() {
-    var sum = p1holes.reduce(function(acc, idx) {
+    var sum = p1holes.reduce(function (acc, idx) {
         return acc + board[idx];
     }, 0);
     if (sum === 0) {
-        board[getPlayersStoreIdx()] += p2holes.reduce(function(acc, idx) {
+        board[getPlayersStoreIdx()] += p2holes.reduce(function (acc, idx) {
             var sum = acc + board[idx];
             board[idx] = 0;
             return sum;
@@ -157,11 +154,11 @@ function checkForClearedRow() {
         getWinner;
         return;
     }
-    sum = p2holes.reduce(function(acc, idx) {
+    sum = p2holes.reduce(function (acc, idx) {
         return acc + board[idx];
     }, 0);
     if (sum === 0) {
-        board[getPlayersStoreIdx()] += p1holes.reduce(function(acc, idx) {
+        board[getPlayersStoreIdx()] += p1holes.reduce(function (acc, idx) {
             var sum = acc + board[idx];
             board[idx] = 0;
             return sum;
@@ -169,31 +166,84 @@ function checkForClearedRow() {
         getWinner();
         return;
     }
-}
-
-function getWinner() {
-    winner = board[p1store] > board[p2store] ? 'Player 1 wins!' : 'Player 2 wins!'
 };
 
-
-
+function getWinner() {
+    winner = board[p1store] > board[p2store] ? 'Player 1 wins!' : 'Player 2 wins!';
+};
 
 function changeTurn(lastHoleIdx) {
     if (turn === 1 && lastHoleIdx === p1store) return;
     if (turn === 2 && lastHoleIdx === p2store) return;
     turn = turn === 1 ? 2 : 1;
-}
+};
 
-
+function highlightHoles(turn) {
+    if (turn === 1) {
+        for (var i = 0; i < p1holes.length; i++) {
+            var elem = p1holes[i];
+            document.getElementById('hole' + elem).style.border = '2px solid blue';
+            document.getElementById('hole' + elem).style.boxShadow = '0px 0px 20px blue';
+            elem = p2holes[i];
+            document.getElementById('hole' + elem).style.border = '';
+            document.getElementById('hole' + elem).style.boxShadow = '';
+        }
+    } else {
+        for (var i = 0; i < p1holes.length; i++) {
+            elem = p2holes[i];
+            document.getElementById('hole' + elem).style.border = '2px solid blue';
+            document.getElementById('hole' + elem).style.boxShadow = '0px 0px 20px blue';
+            elem = p1holes[i];
+            document.getElementById('hole' + elem).style.border = '';
+            document.getElementById('hole' + elem).style.boxShadow = '';
+        }
+    }
+};
 
 function render() {
-    board.forEach(function (numStones, idx) {
-        document.getElementById(`hole${idx}`).textContent = numStones;
-    });
+    // for (var i = 0; i < board.length; i++) {
 
+    // }
+
+
+    board.forEach(function (numStones, idx) {
+        // placeStones(numStones, idx); 
+        // console.log(numStones, idx);
+        document.getElementById(`hole${idx}`).textContent = numStones;
+        
+    });
+    p1scoreDisplay.textContent = `Player 1: ${board[p1store]}`;
+    p2scoreDisplay.textContent = `Player 2: ${board[p2store]}`;
+    if (turn === 1) {
+        document.getElementById('p1-turn').style.border = '2px solid blue';
+        document.getElementById('p1-turn').style.boxShadow = '0px 0px 20px blue';
+        document.getElementById('p2-turn').style.border = '';
+        document.getElementById('p2-turn').style.boxShadow = '';
+        highlightHoles(turn);
+        
+    } else {
+        console.log('turn 2')
+        document.getElementById('p2-turn').style.border = '2px solid blue';
+        document.getElementById('p2-turn').style.boxShadow = '0px 0px 20px blue';
+        document.getElementById('p1-turn').style.border = '';
+        document.getElementById('p1-turn').style.boxShadow = '';
+        highlightHoles(turn);
+    };
 };
 
 
+// function placeStones(numStones, idx) {
 
+//     var parent = document.getElementById(`hole${idx}`);
+//     var img = parent.childNodes[1];
+//     img.src = stones[numStones];
+//     console.log('Hi');
+    // console.log(parent.childNodes);
+    // var img = document.createElement('img')
+    //     img.src = stones[numStones];
+    //     var imgElem = document.getElementById(`hole${idx}`);
+    //     imgElem.appendChild(img);
+    //     console.log('numStones', numStones)
+// }
 
 initialize();
